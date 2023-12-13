6d7b3785217aa01b24a9efad9cae7d596eda4dbc
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-13 16:20:40 +0530

    Update pr_5.3.c

--- pr_5.2.c
+++ pr_5.2.c
@@ -1,3 +1,25 @@
+/*
+	Enter the Size of Row in Array: 3
+
+Enter the Size of Column in Array: 3
+Enter the Value for Element A[0][0]: 2
+Enter the Value for Element A[0][1]: 4
+Enter the Value for Element A[0][2]: 1
+Enter the Value for Element A[1][0]: 3
+Enter the Value for Element A[1][1]: 5
+Enter the Value for Element A[1][2]: 4
+Enter the Value for Element A[2][0]: 8
+Enter the Value for Element A[2][1]: 2
+Enter the Value for Element A[2][2]: 6
+
+The Transpose of Matrix is:
+2 3 8
+4 5 2
+1 4 6
+	*/
+
+
+
 #include<stdio.h>
 void main(){
 	int r,c;

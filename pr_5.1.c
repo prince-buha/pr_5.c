a711b0d1bf337b70c375019991731cf3aa278a9b
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-13 16:15:52 +0530

    Update pr_5.1.c

--- pr_5.1.c
+++ pr_5.1.c
@@ -1,3 +1,24 @@
+/*
+	Enter the Array's Size: 5
+
+Enter value for A[0]: 2
+Enter value for A[1]: -4
+Enter value for A[2]: 1
+Enter value for A[3]: -4
+Enter value for A[4]: -5
+
+The Values of array A are
+Element A[0] 2:
+Element A[1] -4:
+Element A[2] 1:
+Element A[3] -4:
+Element A[4] -5:
+The Negative values of array A are:
+A[1] :-4
+A[3] :-4
+A[4] :-5
+	*/
+
 #include<stdio.h>
 void main(){
 	int n;
